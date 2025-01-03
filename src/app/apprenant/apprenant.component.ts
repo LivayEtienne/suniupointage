b610f2fb3cdfde19a638e69930c73a70a1327fee
khalifa <gayekhalifa99@gmail.com>
2025-01-03 12:20:00 +0000

    recommence la partie sur les apprenants

---
+++ src/app/apprenant/apprenant.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apprenant',
+  imports: [],
+  templateUrl: './apprenant.component.html',
+  styleUrl: './apprenant.component.css'
+})
+export class ApprenantComponent {
+
+}
