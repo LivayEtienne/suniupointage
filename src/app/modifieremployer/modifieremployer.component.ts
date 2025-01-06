0ef1152aecf80b2051e7a6e496e99284543ff9da
livay <livayglound@gmail.com>
2025-01-06 09:14:23 -0800

    commit

---
+++ src/app/modifieremployer/modifieremployer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modifieremployer',
+  imports: [],
+  templateUrl: './modifieremployer.component.html',
+  styleUrl: './modifieremployer.component.css'
+})
+export class ModifieremployerComponent {
+
+}
